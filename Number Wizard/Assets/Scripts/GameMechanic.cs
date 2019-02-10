e103e86795682be1994c6b2968f044a7b14f8fa2
Unknown <99rsw99@mail.ru>
2019-02-10 21:23:04 +0300

    Add number wizard

---
+++ Number Wizard/Assets/Scripts/GameMechanic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameMechanic : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    private int guess;
+
+    void Start()
+    {
+        NextGuess();
+    }
+
+    public void NextScene ()
+    {
+        int _currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(_currentScene + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+    public void PressHigher ()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void PressLower ()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void NextGuess ()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+
+    public void PlayAgain ()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
