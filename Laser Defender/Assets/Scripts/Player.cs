0b1de516f61b1870402072221960cb451c127c9d
Unknown <99rsw99@mail.ru>
2019-02-15 12:54:52 +0300

    Clamp player

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -6,11 +6,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        minX = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        maxX = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        minY = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        maxY = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -21,10 +37,11 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, minX, maxX);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, minY, maxY);
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
