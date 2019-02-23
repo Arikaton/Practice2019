7ae2ebfbce0322e65f55dfaedd7a4154e46dd08e
Unknown <99rsw99@mail.ru>
2019-02-23 15:18:21 +0300

    added UI

---
+++ Glitch Garden/Assets/Scripts/DefenderButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButtons : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        DefenderButtons[] buttons = FindObjectsOfType<DefenderButtons>();
+        foreach (DefenderButtons button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = Color.gray;
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
